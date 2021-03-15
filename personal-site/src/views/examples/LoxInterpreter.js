b6df518fff72bf5bb25c3c9c5ed319385c256801
Noah Peterson <noahbpeterson1997@gmail.com>
2021-03-14 21:03:04 -0500

    Moving LoxInterpreter into its own Component functiion.

--- personal-site/src/views/examples/LoxInterpreter.js
+++ personal-site/src/views/examples/LoxInterpreter.js
@@ -16,8 +16,6 @@
 
 */
 import React, { useState } from "react";
-import classnames from "classnames";
-import { Link } from "react-router-dom";
 // reactstrap components
 import {Button, Card, CardHeader, CardBody,
 		CardFooter, CardTitle, Container, Row, Col } from "reactstrap";
@@ -25,14 +23,11 @@ import {Button, Card, CardHeader, CardBody,
 // core components
 import IndexNavbar from "components/Navbars/IndexNavbar.js";
 import Footer from "components/Footer/Footer.js";
+import LoxInterpreterComponent from "./LoxInterpreterComponent.js";
 
 export default function LoxInterpreter() {
 	const [squares1to6, setSquares1to6] = useState("");
 	const [squares7and8, setSquares7and8] = useState("");
-	const [programText, setProgramText] = useState('print "Hello, world!";');
-	const [programResult, setProgramResult] = useState('');
-	const [programExamples, setProgramExamples] = useState('');
-	const [exampleText, setExampleText] = useState('');
 
 	React.useEffect(() => {
 		document.body.classList.toggle("register-page");
@@ -62,52 +57,6 @@ export default function LoxInterpreter() {
 		"deg)"
 		);
 	};
-	
-	const runProgram = async () => {
-		const result = await fetch(`http://localhost:5000/loxOutput`, {
-			method: 'post',
-			body: JSON.stringify({ text: programText }),
-			headers: {
-				'Content-Type': 'application/json',
-			}
-		});
-		const body = await result.json();
-		setProgramResult(body.trim());
-	}
-	
-	const getExamples = async () => {
-	const result = await fetch('http://localhost:5000/loxExamples', {
-		method: 'get',
-		headers: {
-		'Content-Type': 'application/json',
-		}
-	});
-	const body = await result.json();
-	const items = [];
-	for(const [index, value] of body.entries ()) {
-		
-		items.push(
-			<option value={index} key={index} onClick={() => getExampleText(index)}>{value}</option>
-		)
-	}
-	setProgramExamples(items);
-	}
-
-	const getExampleText = async (chosenExample) => {
-	const result = await fetch('http://localhost:5000/loxExamples/'+chosenExample, {
-		method: 'post',
-		body: JSON.stringify(
-		{ 
-			string: exampleText,
-		}),
-		headers: {
-		'Content-Type': 'application/json',
-		'Accept': 'application/json',
-		}
-	});
-	const body = await result.json();
-	setProgramText(body);
-	}
 
 	return (
 	<>
@@ -142,38 +91,7 @@ export default function LoxInterpreter() {
 							</Card>
 						</Col>
 						<Col>
-							<Card>
-								<CardBody>
-									<textarea 
-										className="form-control-plaintext" 
-										style={{height: '10rem', WebkitFilter: "brightness(1000%)", border: "0.0625rem solid", padding: "0.625rem", fontSize: "1rem", zIndex: 900}}
-										id="input" 
-										rows={10} cols={60} 
-										value= {programText}
-										onChange={(event) => setProgramText(event.target.value)} />
-								</CardBody>
-								<CardBody style={{borderColor: "#8c3db9"}}>
-									<textarea 
-										className="form-control" 
-										style={{borderColor: "#8c3db9", WebkitFilter: "brightness(1000%)", border: "0.0625rem solid", padding: "0.625rem", fontSize: "1rem", zIndex: 900}}
-										id="output" rows={3} cols={50} value={programResult}/>
-								</CardBody>
-							</Card>
-							<Row>
-								<Col>
-									<select onClick={() => getExamples()} className="dropdown-with-icons" aria-labelledby="navbarDropdownMenuLink">
-										<option value={-1} key={-1}>Example Programs</option>
-										{programExamples}
-									</select>
-								</Col>
-								<Col>
-									<Button className="btn-round" color="primary" size="lg" onClick={() => runProgram()}>
-										Run
-									</Button>
-								</Col>
-							</Row>
-									
-									
+							<LoxInterpreterComponent />
 						</Col>
 					</Row>
 
---
+++ personal-site/src/views/examples/LoxInterpreterComponent.js
@@ -0,0 +1,110 @@
+/*!
+
+=========================================================
+* BLK Design System React - v1.2.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/blk-design-system-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/blk-design-system-react/blob/main/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React, { useState } from "react";
+import {Button, Card, CardBody, Row, Col } from "reactstrap";
+
+export default function LoxInterpreterComponent() {
+	const [programText, setProgramText] = useState('print "Hello, world!";');
+	const [programResult, setProgramResult] = useState('');
+	const [programExamples, setProgramExamples] = useState('');
+	const [exampleText, setExampleText] = useState('');
+	
+	const runProgram = async () => {
+		const result = await fetch(`http://localhost:5000/loxOutput`, {
+			method: 'post',
+			body: JSON.stringify({ text: programText }),
+			headers: {
+				'Content-Type': 'application/json',
+			}
+		});
+		const body = await result.json();
+		setProgramResult(body.trim());
+	}
+	
+	const getExamples = async () => {
+		const result = await fetch('http://localhost:5000/loxExamples', {
+			method: 'get',
+			headers: {
+			'Content-Type': 'application/json',
+			}
+		});
+		
+		const body = await result.json();
+		const items = [];
+		for(const [index, value] of body.entries ()) {
+			
+			items.push(
+				<option value={index} key={index} onClick={() => getExampleText(index)}>{value}</option>
+			)
+		}
+		setProgramExamples(items);
+	}
+
+	const getExampleText = async (chosenExample) => {
+		const result = await fetch('http://localhost:5000/loxExamples/'+chosenExample, {
+			method: 'post',
+			body: JSON.stringify(
+			{ 
+				string: exampleText,
+			}),
+			headers: {
+			'Content-Type': 'application/json',
+			'Accept': 'application/json',
+			}
+		});
+		const body = await result.json();
+		setProgramText(body);
+	}
+
+	return (
+	<>
+		<Card>
+			<CardBody>
+				<textarea 
+					className="form-control-plaintext" 
+					style={{height: '10rem', WebkitFilter: "brightness(1000%)", border: "0.0625rem solid", padding: "0.625rem", fontSize: "1rem", zIndex: 900}}
+					id="input" 
+					rows={10} cols={60} 
+					value= {programText}
+					onChange={(event) => setProgramText(event.target.value)} />
+			</CardBody>
+			<CardBody style={{borderColor: "#8c3db9"}}>
+				<textarea 
+					className="form-control" 
+					style={{borderColor: "#8c3db9", WebkitFilter: "brightness(1000%)", border: "0.0625rem solid", padding: "0.625rem", fontSize: "1rem", zIndex: 900}}
+					id="output" rows={3} cols={50} value={programResult}/>
+			</CardBody>
+		</Card>
+		<Row>
+			<Col></Col>
+			<Col>
+				<select onClick={() => getExamples()} className="dropdown-with-icons" aria-labelledby="navbarDropdownMenuLink">
+					<option value={-1} key={-1}>Example Programs</option>
+					{programExamples}
+				</select>
+			</Col>
+			<Col></Col>
+			<Col>
+				<Button className="btn-round" color="primary" size="lg" onClick={() => runProgram()}>
+					Run
+				</Button>
+			</Col>
+		</Row>	
+	</>
+);
+}
