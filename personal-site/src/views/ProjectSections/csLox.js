562c28f29665ce25c3504a0e2ee08c51bf1d7a46
NoahBPeterson <noahbpeterson1997@gmail.com>
2021-03-16 00:27:30 -0500

    Removing unused elements, fixing whitespace.

--- personal-site/src/views/ProjectSections/csLox.js
+++ personal-site/src/views/ProjectSections/csLox.js
@@ -35,93 +35,92 @@ import {
 import LoxInterpreterComponent from "../examples/LoxInterpreterComponent.js";
 
 export default function CsLox() {
-	const [iconTabs, setIconsTabs] = React.useState(1);
 	const [textTabs, setTextTabs] = React.useState(4);
 	return (
 	<div className="section section-tabs">
 		<Container>
-		<div className="title">
-			<h3 className="mb-3">csLox</h3>
-		</div>
-		<Row>
-			<Col className="ml-auto mr-auto" md="10" xl="6">
-				<Card>
-					<CardHeader>
-						<Nav className="nav-tabs-info" role="tablist" tabs>
-							<NavItem>
-								<NavLink
-									className={classnames({
-									active: textTabs === 4,
-									})}
-									onClick={(e) => setTextTabs(4)}
-									style={{cursor: "pointer"}}
-								>
-									Overview
-								</NavLink>
-							</NavItem>
-							<NavItem>
-								<NavLink
-									className={classnames({
-									active: textTabs === 5,
-									})}
-									onClick={(e) => setTextTabs(5)}
-									style={{cursor: "pointer"}}
-								>
-									Language Details
-								</NavLink>
-							</NavItem>
-							<NavItem>
-								<NavLink
-									className={classnames({
-									active: textTabs === 6,
-									})}
-									style={{cursor: "pointer"}}
-									onClick={(e) => setTextTabs(6)}
+			<div className="title">
+				<h1 className="mb-3">csLox</h1>
+			</div>
+			<Row>
+				<Col className="ml-auto mr-auto" md="10" xl="6">
+					<Card>
+						<CardHeader>
+							<Nav className="nav-tabs-info" role="tablist" tabs>
+								<NavItem>
+									<NavLink
+										className={classnames({
+										active: textTabs === 4,
+										})}
+										onClick={(e) => setTextTabs(4)}
+										style={{cursor: "pointer"}}
 									>
-									Website Demo
-								</NavLink>
-							</NavItem>
-						</Nav>
-					</CardHeader>
-					<CardBody>
-						<TabContent className="tab-space" activeTab={"link" + textTabs}>
-							<TabPane tabId="link4">
-								<p>
-									csLox is an interpreter for the Lox programming language.
-									It is a treewalk interpreter, which executes code by traversing the abstract syntax tree.
+										Overview
+									</NavLink>
+								</NavItem>
+								<NavItem>
+									<NavLink
+										className={classnames({
+										active: textTabs === 5,
+										})}
+										onClick={(e) => setTextTabs(5)}
+										style={{cursor: "pointer"}}
+									>
+										Language Details
+									</NavLink>
+								</NavItem>
+								<NavItem>
+									<NavLink
+										className={classnames({
+										active: textTabs === 6,
+										})}
+										style={{cursor: "pointer"}}
+										onClick={(e) => setTextTabs(6)}
+										>
+										Website Demo
+									</NavLink>
+								</NavItem>
+							</Nav>
+						</CardHeader>
+						<CardBody>
+							<TabContent className="tab-space" activeTab={"link" + textTabs}>
+								<TabPane tabId="link4">
+									<p>
+										csLox is an interpreter for the Lox programming language.
+										It is a treewalk interpreter, which executes code by traversing the abstract syntax tree.
 
-								</p>
-							</TabPane>
-							<TabPane tabId="link5">
-								<p>
-									Lox is a dynamically typed functional programming language with support for objects. Variables
-									are statically scoped, and can be one of four types: a floating point number, a boolean, a string,
-									and nil.
-								</p>
-								<p>
-									A full language specification is available here:
-									<a href="https://github.com/NoahBPeterson/csLox">
-										&nbsp;
-										<i className="fab fa-github">NoahBPeterson/csLox</i>		
-									</a>
-								</p>
-							</TabPane>
-							<TabPane tabId="link6">
-								<p>
-									The Lox interpreter program is run by a Node.js application. This app accepts program text 
-									given to it from the demo, which is put into a named file that is used as an argument when 
-									running the interpreter. The output of the interpreter is then sent to the website. It times 
-									out after five seconds to avoid instances of the interpreter never exiting.
-								</p>
-							</TabPane>
-						</TabContent>
-					</CardBody>
-				</Card>
-			</Col>
-			<Col className="ml-auto mr-auto" md="10" xl="6">
-				<LoxInterpreterComponent />
-			</Col>
-		</Row>
+									</p>
+								</TabPane>
+								<TabPane tabId="link5">
+									<p>
+										Lox is a dynamically typed functional programming language with support for objects. Variables
+										are statically scoped, and can be one of four types: a floating point number, a boolean, a string,
+										and nil.
+									</p>
+									<p>
+										A full language specification is available here:
+										<a href="https://github.com/NoahBPeterson/csLox">
+											&nbsp;
+											<i className="fab fa-github"><i style={{fontSize: "0.5rem"}}>&nbsp;</i>NoahBPeterson/csLox</i>		
+										</a>
+									</p>
+								</TabPane>
+								<TabPane tabId="link6">
+									<p>
+										The Lox interpreter program is run by a Node.js application. This app accepts program text 
+										given to it from the demo, which is put into a named file that is used as an argument when 
+										running the interpreter. The output of the interpreter is then sent to the website. It times 
+										out after five seconds to avoid instances of the interpreter never exiting.
+									</p>
+								</TabPane>
+							</TabContent>
+						</CardBody>
+					</Card>
+				</Col>
+				<Col className="ml-auto mr-auto" md="10" xl="6">
+					<LoxInterpreterComponent />
+				</Col>
+			</Row>
 		</Container>
 	</div>
 	);
