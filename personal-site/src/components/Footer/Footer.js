32b9a90e937b42778a6d6bf037d36aa7e982a5ab
NoahBPeterson <noahbpeterson1997@gmail.com>
2021-03-16 00:19:30 -0500

    Changing icons and site map to match my usage.

--- personal-site/src/components/Footer/Footer.js
+++ personal-site/src/components/Footer/Footer.js
@@ -34,10 +34,10 @@ export default function Footer() {
     <footer className="footer">
       <Container>
         <Row>
-          <Col md="1">
-            <h1 className="title">BLK•</h1>
+          <Col md="1.5">
+            <h2 className="title">Site Map</h2>
           </Col>
-          <Col md="1">
+          <Col md="3">
             <Nav>
               <NavItem>
                 <NavLink to="/" tag={Link}>
@@ -50,12 +50,13 @@ export default function Footer() {
                 </NavLink>
               </NavItem>
               <NavItem>
-                <NavLink to="/profile-page" tag={Link}>
-                  Profile
+                <NavLink to="/loxInterpreter" tag={Link}>
+                  Lox Interpreter
                 </NavLink>
               </NavItem>
             </Nav>
           </Col>
+          {/*
           <Col md="3">
             <Nav>
               <NavItem>
@@ -70,44 +71,34 @@ export default function Footer() {
               </NavItem>
             </Nav>
           </Col>
+          */}
+          <Col></Col>
           <Col md="3">
-            <h3 className="title">Follow us:</h3>
+            <h1 className="title">Contact:</h1>
             <div className="btn-wrapper profile">
               <Button
-                className="btn-icon btn-neutral btn-round btn-simple"
+                className="btn-icon btn-neutral btn-round btn-simple "
                 color="default"
-                href="https://twitter.com/creativetim"
+                href="https://www.linkedin.com/in/noah-peterson-a1b0a1149/"
                 id="tooltip622135962"
                 target="_blank"
               >
-                <i className="fab fa-twitter" />
+                <i className="fab fa-linkedin" />
               </Button>
               <UncontrolledTooltip delay={0} target="tooltip622135962">
-                Follow us
+                Connect with me
               </UncontrolledTooltip>
               <Button
                 className="btn-icon btn-neutral btn-round btn-simple"
                 color="default"
-                href="https://www.facebook.com/creativetim"
+                href="https://github.com/NoahBPeterson"
                 id="tooltip230450801"
                 target="_blank"
               >
-                <i className="fab fa-facebook-square" />
+                <i className="fab fa-github" />
               </Button>
               <UncontrolledTooltip delay={0} target="tooltip230450801">
-                Like us
-              </UncontrolledTooltip>
-              <Button
-                className="btn-icon btn-neutral btn-round btn-simple"
-                color="default"
-                href="https://dribbble.com/creativetim"
-                id="tooltip318450378"
-                target="_blank"
-              >
-                <i className="fab fa-dribbble" />
-              </Button>
-              <UncontrolledTooltip delay={0} target="tooltip318450378">
-                Follow us
+                Check out my code
               </UncontrolledTooltip>
             </div>
           </Col>
