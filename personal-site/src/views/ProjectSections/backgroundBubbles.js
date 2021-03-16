cb79b417aa2bc14c37c95f1e5403d8bf6e793764
NoahBPeterson <noahbpeterson1997@gmail.com>
2021-03-16 00:19:14 -0500

    Moving background bubbles into a separate component.

---
+++ personal-site/src/views/ProjectSections/backgroundBubbles.js
@@ -0,0 +1,12 @@
+export default function BackgroundBubbles() {
+    return (
+        <div className="section-javascript">
+            <img alt="..." className="path"
+                src={require("assets/img/path5.png").default}
+            />
+            <img alt="..." className="path path1"
+                src={require("assets/img/path5.png").default}
+            />
+        </div>
+    );
+}
