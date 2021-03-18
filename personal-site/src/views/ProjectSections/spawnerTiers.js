506ca98a2d774329010278c93052575e4d7d55f8
Noah Peterson <noahbpeterson1997@gmail.com>
2021-03-18 01:59:53 -0500

    Finishing the SpawnerTiers.js Project Section

--- personal-site/src/views/ProjectSections/spawnerTiers.js
+++ personal-site/src/views/ProjectSections/spawnerTiers.js
@@ -69,7 +69,7 @@ export default function SpawnerTiers() {
 										onClick={(e) => setTextTabs(5)}
 										style={{cursor: "pointer"}}
 									>
-										Implementation Details
+										Details
 									</NavLink>
 								</NavItem>
 							</Nav>
@@ -78,28 +78,21 @@ export default function SpawnerTiers() {
 							<TabContent className="tab-space" activeTab={"link" + textTabs}>
 								<TabPane tabId="link4">
 									<p>
-										BlockTrack is an administrative tool for use in Minecraft: Bedrock Edition servers.
-										It gives administrators the ability to track block placement history and user
-										history to combat bad behavior.
+										SpawnerTiers is a plugin for use in Minecraft: Bedrock Edition servers.
+										It makes spawners upgradeable, and gives administrators the ability to give players 
+										a monster spawner.
 									</p>
 								</TabPane>
 								<TabPane tabId="link5">
 									<p>
-										There are event handlers triggered when blocks are placed or broken. This adds a
-										new history record with the player's username, the datetime of the action, and the
-										location of the block. The database calls are done asynchronously using the server
-										scheduler to avoid making blocking I/O calls on the main thread.
-											
-									</p>
-									<p>
 										The source repository is available here:
-										<a href="https://github.com/NoahBPeterson/BlockTrack">
+										<a href="https://github.com/NoahBPeterson/SpawnerTiers">
 											&nbsp;
 											<i className="fab fa-github">
 												<i style={{fontSize: "0.5rem"}}>
 													&nbsp;
 												</i>
-												NoahBPeterson/BlockTrack
+												NoahBPeterson/SpawnerTiers
 											</i>		
 										</a>
 									</p>
@@ -116,7 +109,7 @@ export default function SpawnerTiers() {
 				</Col>
 				<Col className="ml-auto mr-auto" md="10" xl="6">
 					<iframe width="560" height="315" 
-						src="https://www.youtube.com/embed/SZdcB1AAgZI" frameborder="0"
+						src="https://www.youtube.com/embed/lR_137aqd8U" frameborder="0"
 						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
 						allowFullScreen>
 					</iframe>
